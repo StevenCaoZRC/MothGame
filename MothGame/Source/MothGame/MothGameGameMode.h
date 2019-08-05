2c50d4ade68232b162fe10a6291b1f04fe9cab7b
Steven <stevenzhaooooo@gmail.com>
2019-08-05 14:57:49 +1200

    initial project

---
+++ MothGame/Source/MothGame/MothGameGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MothGameGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AMothGameGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AMothGameGameMode();
+};
+
+
+
